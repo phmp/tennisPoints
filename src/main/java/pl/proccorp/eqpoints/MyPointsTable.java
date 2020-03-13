d1ab21cbc5b02f4b073d84a4db6a88db4587275f
Paweł Proc <pawel.henryk.proc@gmail.com>
2020-03-13 21:55:29 +0100

    REFACTOR

--- src/main/java/pl/proccorp/eqpoints/MyPointsTable.java
+++ src/main/java/pl/proccorp/eqpoints/MyPointsTable.java
@@ -1,14 +1,12 @@
 package pl.proccorp.eqpoints;
 
-import java.text.MessageFormat;
 import java.util.List;
 
 public class MyPointsTable implements PointsTable {
 
-    private List<String> pointsView = List.of("0","15","30","40");
+    private List<String> pointsView = List.of("0", "15", "30", "40");
     private int pointsOfPlayerA = 0;
     private int pointsOfPlayerB = 0;
-    private String displayPattern = "0/0 0/0 {0}/{1}";
 
     public void playerAWonPoint() {
         pointsOfPlayerA++;
@@ -21,6 +19,6 @@ public class MyPointsTable implements PointsTable {
     public String currentScore() {
         String playerAScore = pointsView.get(pointsOfPlayerA);
         String playerBScore = pointsView.get(pointsOfPlayerB);
-        return MessageFormat.format(displayPattern, playerAScore, playerBScore);
+        return "0/0 0/0 " + playerAScore + '/' + playerBScore;
     }
 }
--- src/test/java/pl/proccorp/eqpoints/MyPointsTableTest.java
+++ src/test/java/pl/proccorp/eqpoints/MyPointsTableTest.java
@@ -1,52 +1,41 @@
 package pl.proccorp.eqpoints;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
 
 class MyPointsTableTest {
 
-    @Test
-    void initScoreShouldBeCorrect() {
-        PointsTable table = new MyPointsTable();
-        String score = table.currentScore();
-        Assertions.assertThat(score).isEqualTo("0/0 0/0 0/0");
+    static List<Arguments> list() {
+        return List.of(
+                Arguments.of(0, "0/0 0/0 0/0"),
+                Arguments.of(1, "0/0 0/0 15/0"),
+                Arguments.of(2, "0/0 0/0 30/0"),
+                Arguments.of(3, "0/0 0/0 40/0")
+//                Arguments.of(4, "0/0 1/0 0/0"),
+//                Arguments.of(5, "0/0 1/0 15/0"),
+//                Arguments.of(6, "0/0 1/0 30/0"),
+//                Arguments.of(7, "0/0 1/0 40/0"),
+//                Arguments.of(8, "0/0 2/0 0/0")
+        );
     }
 
-    @Test
-    void score15() {
+    @ParameterizedTest
+    @MethodSource("list")
+    void initScoreShouldBeCorrect(int numberOfPointsWonByA, String expectedResult) {
         PointsTable table = new MyPointsTable();
-        table.playerAWonPoint();
+        addPointsForPlayerA(table, numberOfPointsWonByA);
         String score = table.currentScore();
-        Assertions.assertThat(score).isEqualTo("0/0 0/0 15/0");
+        Assertions.assertThat(score).isEqualTo(expectedResult);
     }
 
-    @Test
-    void score30() {
-        PointsTable table = new MyPointsTable();
-        table.playerAWonPoint();
-        table.playerAWonPoint();
-        String score = table.currentScore();
-        Assertions.assertThat(score).isEqualTo("0/0 0/0 30/0");
+    private void addPointsForPlayerA(PointsTable table, int numberOfPoints) {
+        for (int i = 0; i < numberOfPoints; i++) {
+            table.playerAWonPoint();
+        }
     }
 
-    @Test
-    void score40() {
-        PointsTable table = new MyPointsTable();
-        table.playerAWonPoint();
-        table.playerAWonPoint();
-        table.playerAWonPoint();
-        String score = table.currentScore();
-        Assertions.assertThat(score).isEqualTo("0/0 0/0 40/0");
-    }
-
-    @Test
-    void winGame() {
-        PointsTable table = new MyPointsTable();
-        table.playerAWonPoint();
-        table.playerAWonPoint();
-        table.playerAWonPoint();
-        table.playerAWonPoint();
-        String score = table.currentScore();
-        Assertions.assertThat(score).isEqualTo("0/0 1/0 0/0");
-    }
 }
