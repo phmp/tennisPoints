2793b091247e76183f3432f75ee196a3806477e7
Paweł Proc <pawel.henryk.proc@gmail.com>
2020-03-13 19:45:49 +0100

    GREEN points in one game can be increment and view in correct format

--- src/main/java/pl/proccorp/eqpoints/MyPointsTable.java
+++ src/main/java/pl/proccorp/eqpoints/MyPointsTable.java
@@ -1,18 +1,26 @@
 package pl.proccorp.eqpoints;
 
+import java.text.MessageFormat;
+import java.util.List;
+
 public class MyPointsTable implements PointsTable {
 
-    private String current = "0/0 0/0 0/0";
+    private List<String> pointsView = List.of("0","15","30","40");
+    private int pointsOfPlayerA = 0;
+    private int pointsOfPlayerB = 0;
+    private String displayPattern = "0/0 0/0 {0}/{1}";
 
     public void playerAWonPoint() {
-
+        pointsOfPlayerA++;
     }
 
     public void playerBWonPoint() {
-
+        pointsOfPlayerB++;
     }
 
     public String currentScore() {
-        return current;
+        String playerAScore = pointsView.get(pointsOfPlayerA);
+        String playerBScore = pointsView.get(pointsOfPlayerB);
+        return MessageFormat.format(displayPattern, playerAScore, playerBScore);
     }
 }
