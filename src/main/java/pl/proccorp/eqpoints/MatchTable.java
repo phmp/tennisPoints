59768b7141639ab897548f811158f09658a93de3
Paweł Proc <pawel.henryk.proc@gmail.com>
2020-03-14 23:09:09 +0100

    GREEN new class for sets

--- src/main/java/pl/proccorp/eqpoints/MatchTable.java
+++ src/main/java/pl/proccorp/eqpoints/MatchTable.java
@@ -3,29 +3,37 @@ package pl.proccorp.eqpoints;
 import pl.proccorp.eqpoints.general.ScoreTable;
 import pl.proccorp.eqpoints.specyfic.PointsTable;
 import pl.proccorp.eqpoints.specyfic.GamesTable;
+import pl.proccorp.eqpoints.specyfic.SetsTable;
 
 public class MatchTable implements ScoreTable {
 
     private ScoreTable pointsTable = new PointsTable();
     private ScoreTable gamesTable = new GamesTable();
+    private ScoreTable setsTable = new SetsTable();
 
     public boolean playerAWonPoint() {
         if (pointsTable.playerAWonPoint()) {
-            gamesTable.playerAWonPoint();
             pointsTable = new PointsTable();
+            if (gamesTable.playerAWonPoint()){
+                gamesTable = new GamesTable();
+                return setsTable.playerAWonPoint();
+            }
         }
         return false;
     }
 
     public boolean playerBWonPoint() {
         if (pointsTable.playerBWonPoint()) {
-            gamesTable.playerBWonPoint();
             pointsTable = new PointsTable();
+            if (gamesTable.playerBWonPoint()){
+                gamesTable = new GamesTable();
+                return setsTable.playerBWonPoint();
+            }
         }
         return false;
     }
 
     public String currentScore() {
-        return "0/0 " + gamesTable.currentScore() + " " + pointsTable.currentScore();
+        return setsTable.currentScore() + " " + gamesTable.currentScore() + " " + pointsTable.currentScore();
     }
 }
--- src/main/java/pl/proccorp/eqpoints/specyfic/SetsTable.java
+++ src/main/java/pl/proccorp/eqpoints/specyfic/SetsTable.java
@@ -2,9 +2,8 @@ package pl.proccorp.eqpoints.specyfic;
 
 import pl.proccorp.eqpoints.general.SimpleScoreTable;
 
-public class GamesTable extends SimpleScoreTable {
-
-    public GamesTable() {
-        super(7);
+public class SetsTable extends SimpleScoreTable {
+    public SetsTable() {
+        super(3);
     }
 }
